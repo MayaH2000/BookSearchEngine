c72f53df30107f01bb26f215aa4534b58c56483d
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-02-26 21:46:08 -0600

    imported apollo server correctly, created schemas folder containing index.js, resolvers.js, typedefs.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express'); // Import ApolloServer
+const { ApolloServer } = require('@apollo/server'); // Import ApolloServer
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
