113bbb09a1117affbc785611c59b1f22e061d555
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-02-26 21:35:00 -0600

    import ApolloServer, added Apollo Server Middleware to Express

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express'); // Import ApolloServer
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas'); // Import GraphQL type definitions and resolvers
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +15,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const server = new ApolloServer({
+  typeDefs, // Pass your GraphQL type definitions
+  resolvers, // Pass your resolvers
+});
+
+// Apply Apollo Server middleware to Express
+server.applyMiddleware({ app });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
