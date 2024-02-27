aa1fee2f226eac0920a6f7f87956c0583852d58d
MayaH2000 <101356128+MayaH2000@users.noreply.github.com>
2024-02-26 22:19:59 -0600

    Create an Apollo Provider to make every request work with the Apollo server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// Create an instance of ApolloClient
+const client = new ApolloClient({
+  uri: 'your_graphql_endpoint_here',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    // Wrap your application with the ApolloProvider
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
